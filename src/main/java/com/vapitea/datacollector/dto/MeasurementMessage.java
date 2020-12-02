2d34ad8fa228cab4ccc22007b343e0d96025637a
Milan Gyuricska <vapitea@gmail.com>
2020-12-02 19:18:08 +0100

    Merge spring boot project with angular project

--- src/main/java/com/vapitea/datacollector/dto/MeasurementMessage.java
+++ src/main/java/com/vapitea/datacollector/dto/MeasurementMessage.java
@@ -3,6 +3,7 @@ package com.vapitea.datacollector.dto;
 
 import lombok.Data;
 
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.Map;
@@ -10,9 +11,9 @@ import java.util.Map;
 @Data
 public class MeasurementMessage {
 
-    @NotNull
-    @NotEmpty
-    private String uuid;
-    @NotNull
-    private Map<String, Double> payload;
+  @NotNull
+  @NotEmpty
+  private String uuid;
+  @NotNull
+  private Map<String, Double> payload;
 }
